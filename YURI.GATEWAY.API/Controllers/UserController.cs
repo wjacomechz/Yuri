620ead6efaf528d9f580d0db47cb2e94668cb027
wjacomechz <wjacomechz@gmail.com>
2022-10-15 09:18:32 -0500

    wjacome - api gateway

---
+++ YURI.GATEWAY.API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YURI.GATEWAY.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var clave = "xxx";
+            return Ok(clave);
+        }
+
+    }
+}
